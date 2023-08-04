856f944f99d81216148433a5e80101adbd3541b3
Gabriel Lucas <gabriel.portela@ime.eb.br>
2023-08-04 19:01:42 -0400

    commit atualização

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,15 +1,5 @@
 <h3>Fornecedor</h3>
 <p>Aqui é a página do fornecedor</p>
-{{-- Aqui pode colocar o comentário, será descartado pelo Blade --}}
-{{ 'Isso aqui é a mesma coisa que a sintaxe de impressão PHP.' }}
-@php
-    //Para comentários dentro do PHP
-    /*
-    Para 
-    Várias
-    Linhas
-    */
-@endphp
 
 @if(count($fornecedores) > 0 && count($fornecedores) < 10)
     <h3>Existem alguns fornecedores cadastrados</h3>
@@ -19,18 +9,75 @@
     <h3>Não existem fornecedores cadastrados</h3>
 @endif
 
-Fornecedor: {{ $fornecedores[0]['nome'] }}
+@isset($fornecedores)
+    <hr>
+    @forelse($fornecedores as $indice => $fornecedor)
+        Fornecedor: {{ $fornecedor['nome'] }}
+        <br>
+        Status: {{ $fornecedor['status'] }}
+        <br>
+        CNPJ: {{ $fornecedor['cnpj'] ?? 'CNPJ não informado!'}}
+        <br>
+        Telefone: ({{ $fornecedor['ddd'] ?? ''}}) {{ $fornecedor['telefone'] }}
+        <br>
+        @switch($fornecedor['ddd'])
+            @case('11')
+                São Paulo - SP
+                @break
+            @case('85')
+                Fortaleza - CE
+                @break
+            @case('32')
+                Juiz de Fora - MG
+                @break
+            @default
+                Estado não identificado
+        @endswitch
+        <hr>
+    @empty
+        Não existem fornecedores cadastrados!
+    @endforelse
+@endisset
+
+{{-- @for($i = 0; $i < count($fornecedores); $i++)
+@endfor --}}
+
+{{-- Caso o campo 'cnpj' do array não existir ou tiver valor null --> usa default --}}
+{{-- Fornecedor: {{ $fornecedores[0]['nome'] }}
 <br>
 
 Status: {{ $fornecedores[0]['status'] }}
-<br>
+<br> --}}
 
+{{-- @isset($fornecedores)
+    Fornecedor: {{ $fornecedores[0]['nome'] }}
+    <br>
+    Status: {{ $fornecedores[0]['status'] }}
+    <br>
+    @isset($fornecedores[0]['cnpj'])
+        CNPJ: {{ $fornecedores[0]['cnpj'] }}
+        @empty($fornecedores[0]['cnpj'])
+            - Vazio
+        @endempty
+    @endisset
+@endisset --}}
+
+{{-- 
 @if(!($fornecedores[0]['status'] == 'S'))
     <h3>Fornecedor Inativo</h3>
 @endif
 
 @unless($fornecedores[0]['status'] == 'S')
     <h3>Fornecedor Inativo</h3>
-@endunless
+@endunless --}}
 
-@dd($fornecedores)
+{{-- @dd($fornecedores) --}}
+
+{{-- 
+    1 - @dd() serve para imprimir arrays 
+    2 - @isset() serve para saber se uma variável ou campo de array está setado
+    3 - @if()/@elseif() --> apenas o if/else normal
+    4 - @unless() --> executa quando é falso
+    5 - @empty() vale True quando '', 0, 0.0, '0', null, false, array(), $var declarada, mas não inicializada.
+--}}
+{{-- Aqui pode colocar o comentário, será descartado pelo Blade --}}
