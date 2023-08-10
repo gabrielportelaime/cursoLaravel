692f88d6366824eb467a0d7e13b8c81ac12ed24d
Gabriel Portela <gabriel.portela@ime.eb.br>
2023-08-10 12:13:52 -0400

    soft deletes --> deleted_at

--- app/Models/Fornecedor.php
+++ app/Models/Fornecedor.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 //Fornecedor
 //fornecedors --> ou seja, vai dar erro, porque a tabela não corresponde
@@ -12,6 +13,7 @@ class Fornecedor extends Model
 {
     protected $table = 'fornecedores';
     protected $fillable = ['nome', 'site', 'uf', 'email'];
+    use SoftDeletes;
     //Para poder persistir os dados corretamente, pois a geração automática da tabela seria fornecedors --> errado
     use HasFactory;
 }
--- app/Models/SiteContato.php
+++ app/Models/SiteContato.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 //Site_Contato
 //site_contato
