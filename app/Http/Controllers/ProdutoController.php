e10770db57ceebb31bb63a97bcece58e56626df1
Gabriel Portela <gabriel.portela@ime.eb.br>
2023-08-16 12:43:36 -0400

    CRUD de produtos finalizado

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Produto;
+use App\Models\Unidade;
 use Illuminate\Http\Request;
 
 class ProdutoController extends Controller
@@ -10,17 +11,19 @@ class ProdutoController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        echo 'Produto - index';
+        $produtos = Produto::paginate(10);
+        return view('app.produto.index', ['produtos' => $produtos, 'request' =>  $request->all()]);
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(Request $request)
     {
-        echo 'Produto - Criar';
+        $unidades = Unidade::all();
+        return view('app.produto.create', ['unidades' => $unidades]);
     }
 
     /**
@@ -28,7 +31,24 @@ class ProdutoController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $regras = [
+            'nome' => 'required|min:3|max:40',
+            'descricao' => 'required|min:3|max:40',
+            'peso' => 'required|integer',
+            'unidade_id' => 'exists:unidades,id',
+        ];
+        $feedbacks = [
+            'required' => 'O campo :attribute deve ser preenchido!',
+            'nome.min' => 'O campo nome deve ter no mínimo 3 caracteres!',
+            'nome.max' => 'O campo nome deve ter no máximo 40 caracteres!',
+            'descricao.min' => 'O campo descrição deve ter no mínimo 3 caracteres!',
+            'descricao.max' => 'O campo descrição deve ter no máximo 2000 caracteres!',
+            'peso.integer' => 'O campo peso deve um número inteiro!',
+            'unidade_id.exists' => 'A unidade de medida informada não existe!',
+        ];
+        $request->validate($regras, $feedbacks);
+        Produto::create($request->all());
+        return redirect()->route('produto.index');
     }
 
     /**
