9805ae12d33fe114eb865ddc4ed2c742ce0f81c2
Gabriel Portela <gabriel.portela@ime.eb.br>
2023-08-17 16:51:32 -0400

    pedido, pedido-produto, clientes

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal'])->name('site.index')->middleware('log.acesso');
 Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato'])->name('site.contato');
 Route::post('/contato', [\App\Http\Controllers\ContatoController::class, 'salvar'])->name('site.contato');
+Route::get('/teste/{p1}/{p2}', [\App\Http\Controllers\TesteController::class, 'teste'])->name('teste');
+Route::fallback(function(){
+    echo 'A rota acessada não existe. <a href="'.route('site.index').'">Clique aqui</a> para retornar a página principal!';
+});
 Route::get('/contato/{nome}/{assunto}/{categoria_id?}', 
     function(
         string $nome, 
@@ -12,15 +16,13 @@ Route::get('/contato/{nome}/{assunto}/{categoria_id?}',
         string $mensagem = 'Mensagem não informada!'){
     echo "Sua solicitação: $nome - $assunto - $mensagem";
 })->where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+');
-Route::get('/sobre', [\App\Http\Controllers\SobreNosController::class, 'sobre'])->name('site.sobrenos');
 
+Route::get('/sobre', [\App\Http\Controllers\SobreNosController::class, 'sobre'])->name('site.sobrenos');
 Route::get('/login/{erro?}', [\App\Http\Controllers\LoginController::class, 'index'])->name('site.login');
 Route::post('/login', [\App\Http\Controllers\LoginController::class, 'autenticar'])->name('site.login');
-
 Route::middleware('autenticacao:padrao,visitante')->prefix('/app')->group(function(){
     Route::get('/home', [\App\Http\Controllers\HomeController::class, 'index'])->name('app.home');
     Route::get('/sair', [\App\Http\Controllers\LoginController::class, 'sair'])->name('app.sair');
-    Route::get('/cliente', [\App\Http\Controllers\ClienteController::class, 'index'])->name('app.cliente');
     Route::get('/fornecedor', [\App\Http\Controllers\FornecedorController::class, 'index'])->name('app.fornecedor');
     Route::post('/fornecedor/listar', [\App\Http\Controllers\FornecedorController::class, 'listar'])->name('app.fornecedor.listar');
     Route::get('/fornecedor/listar', [\App\Http\Controllers\FornecedorController::class, 'listar'])->name('app.fornecedor.listar');
@@ -32,14 +34,9 @@ Route::middleware('autenticacao:padrao,visitante')->prefix('/app')->group(functi
     Route::resource('produto', App\Http\Controllers\ProdutoController::class);
     //produto detalhes
     Route::resource('produto-detalhe', App\Http\Controllers\ProdutoDetalheController::class);
+    Route::resource('cliente', App\Http\Controllers\ClienteController::class);
+    Route::resource('pedido', App\Http\Controllers\PedidoController::class);
+    Route::resource('pedido-produto', App\Http\Controllers\PedidoProdutoController::class);
 });
 
-Route::get('/teste/{p1}/{p2}', [\App\Http\Controllers\TesteController::class, 'teste'])->name('teste');
 
-Route::fallback(function(){
-    echo 'A rota acessada não existe. <a href="'.route('site.index').'">Clique aqui</a> para retornar a página principal!';
-});
-
-// Route::get('/sobre', function () {return 'Sobre';});
-// Route::get('/contato', function () {return 'Contato';});
-// Verbos http get, post, put, patch, delete, options
